68636f5491be27d5b4e0f7dd855ee7853ecfd5c2
liamdichiro <liam.dichiro@mail.mcgill.ca>
2024-02-18 23:02:21 -0500

    check for registration test

--- src/test/java/ca/mcgill/ecse321/SportsCenterApp/repository/SessionRepositoryTests.java
+++ src/test/java/ca/mcgill/ecse321/SportsCenterApp/repository/SessionRepositoryTests.java
@@ -1,3 +1,22 @@
+package ca.mcgill.ecse321.SportsCenterApp.repository;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import ca.mcgill.ecse321.SportsCenterApp.model.Customer;
+import ca.mcgill.ecse321.SportsCenterApp.model.Instructor;
+
+@SpringBootTest
+@ActiveProfiles("test")
+
 public class SessionRepositoryTests {
     
 }
