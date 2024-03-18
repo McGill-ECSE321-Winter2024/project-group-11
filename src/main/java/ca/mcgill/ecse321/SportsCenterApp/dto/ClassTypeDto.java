4e877cf513b242f072148891849a271ab036127c
Seungyeon Lee <seungyeon.lee3@mail.mcgill.ca>
2024-03-18 16:28:23 -0400

    Implemented session api, still working

--- src/main/java/ca/mcgill/ecse321/SportsCenterApp/dto/ClassTypeDto.java
+++ src/main/java/ca/mcgill/ecse321/SportsCenterApp/dto/ClassTypeDto.java
@@ -8,41 +8,45 @@ public class ClassTypeDto {
     private String description;
     private boolean approved;
     private DifficultyLevel difficultyLevel;
-
+    private Integer id;
 
     public ClassTypeDto(){
 
     }
 
-    public ClassTypeDto(String name, String description, boolean approved, DifficultyLevel difficultyLevel){
+    public ClassTypeDto(String name, String description, boolean approved, DifficultyLevel difficultyLevel, Integer id){
         this.name = name;
         this.description = description;
         this.approved = approved;
         this.difficultyLevel = difficultyLevel;
+        this.id = id;
     }
-    
 
-     
-     public String getName() {
+
+
+    public String getName() {
         return name;
     }
 
-    
+    public Integer getId() {
+        return id;
+    }
+
     public void setName(String name) {
         this.name = name;
     }
 
-    
+
     public String getDescription() {
         return description;
     }
 
-    
+
     public void setDescription(String description) {
         this.description = description;
     }
 
-  
+
     public boolean isApproved() {
         return approved;
     }
@@ -52,12 +56,12 @@ public class ClassTypeDto {
         this.approved = approved;
     }
 
- 
+
     public DifficultyLevel getDifficultyLevel() {
         return difficultyLevel;
     }
 
-  
+
     public void setDifficultyLevel(DifficultyLevel difficultyLevel) {
         this.difficultyLevel = difficultyLevel;
     }
