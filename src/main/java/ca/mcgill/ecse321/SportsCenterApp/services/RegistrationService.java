c94360da80d55b210b9159e52c1d03dd6e060289
liamdichiro <liam.dichiro@mail.mcgill.ca>
2024-03-18 00:30:22 -0400

    Updating work

--- src/main/java/ca/mcgill/ecse321/SportsCenterApp/services/RegistrationService.java
+++ src/main/java/ca/mcgill/ecse321/SportsCenterApp/services/RegistrationService.java
@@ -13,12 +13,39 @@ import java.util.*;
 public class RegistrationService {
     
     @Autowired
-    private RegistrationService registrationRepository;
+    private RegistrationRepository registrationRepository;
 
     @Transactional
     public Registration createRegistration(Date aDate, Time aTime, Customer aCustomer, Session aSession){
 
+        Registration registration = new Registration(aDate, aTime, aCustomer, aSession);
+
+        registrationRepository.save(registration);
+
+        return registration;
     }
+
+    @Transactional
+    public Registration getRegistration(Integer id){
+        Optional<Registration> registration = registrationRepository.findById(id);
+        if (registration.isPresent()){
+            return registration.get();
+        }
+        else {
+            throw new IllegalArgumentException("Registration not found for id: " + id);
+        }
+    }
+
+
+    
+
+
+    
+    @Transactional
+    public void deleteRegistration(Integer id){
+        registrationRepository.deleteById(id);
+    }
+
 
 
 }
