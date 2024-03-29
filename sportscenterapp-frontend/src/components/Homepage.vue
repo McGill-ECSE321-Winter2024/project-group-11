00c856fc1c62e6de23c2b822f383b5b18b78f708
Dramocrystal <asselahyazid@gmail.com>
2024-03-29 18:59:51 -0400

    basic homepage

---
+++ sportscenterapp-frontend/src/components/Homepage.vue
@@ -0,0 +1,110 @@
+<template>
+    <div class="homepage">
+      <navbar />  
+      <div class="content">
+        <img src="@/assets/manworkout.jpg" class="background-img">
+        <div class="img-text">Welcome to the sports center</div>
+        <div class="white-section">
+          <div class="card-row">
+            <card 
+              v-for="(cardData, index) in cardDataList"
+              :key="index"
+              :imageSrc="cardData.imageSrc"
+              :title="cardData.title"
+              :description="cardData.description"
+              class="card"
+            />
+          </div>
+        </div>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  import navbar from '@/components/Navbar'
+  import card from '@/components/Card' 
+  import catImage from '@/assets/cat1.jpg'
+  import catImage2 from '@/assets/cat2.jpg'
+  
+  export default {
+    name: 'homepage',
+    components: {
+      navbar,
+      card
+    },
+    data() {
+      return {
+        msg: 'Welcome to the sports center',
+        cardDataList: [
+          {
+            imageSrc: catImage,
+            title: 'Card 1',
+            description: 'Description for Card 1'
+          },
+          {
+            imageSrc: catImage2,
+            title: 'Card 2',
+            description: 'Description for Card 2'
+          },
+          // Add more card data as needed
+        ]
+      }
+    }
+  }
+  </script>
+
+<style>
+
+body, html {
+    overflow-x: hidden;
+}
+
+.homepage {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+.content {
+    position: relative;
+    width: 100%;
+    height: 100%;
+}
+
+.background-img {
+    width: 100%;
+    height: 60%;
+    object-fit: cover;
+}
+
+.white-section {
+    position: absolute;
+    bottom: 0;
+    left: 0;
+    width: 100%;
+    height: 40%;
+    background-color: white;
+    display: flex;
+    justify-content: space-around; /* Align cards horizontally */
+    align-items: center; /* Align cards vertically */
+}
+
+.img-text {
+    position: absolute;
+    top: 24px;
+    left: 30%;
+    color: white;
+    font-weight: bold;
+    font-size: 4vw;
+    width: 100%;
+}
+
+.card-row {
+    display: flex;
+    justify-content: space-around;
+}
+
+.card {
+    margin-right: 24px;
+}
+</style>
---
+++ sportscenterapp-frontend/src/components/Navbar.vue
@@ -0,0 +1,63 @@
+<template>
+    <div class="navbar">
+      <img src="@/assets/logo.jpg" alt="logo" class="logo">
+      <div class="nav-links">
+        <router-link to="/" class="nav-link">Home</router-link>
+        <router-link to="/about" class="nav-link">About</router-link>
+        <router-link to="/classes" class="nav-link">Classes</router-link>
+        <router-link to="/gallery" class="nav-link">Gallery</router-link>
+        <router-link to="/enroll" class="nav-link enroll-button">Enroll Now</router-link>
+      </div>
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'navbar'
+  }
+  </script>
+  
+  <style scoped>
+  .navbar {
+    display: flex;
+    justify-content: space-between; /* Move the logo to the left */
+    align-items: center;
+    background-color: #01161e;
+    padding: 20px;
+  }
+  
+  .logo {
+    width: 64px;
+    border-radius: 3px;
+  }
+  
+  .nav-links {
+    display: flex;
+  }
+  
+  .nav-link {
+    color: white;
+    text-decoration: none;
+    margin-right: 0px;
+    padding: 8px 16px;
+    transition: 0.3s;
+  }
+  
+  .nav-link:hover {
+    background-color: #90e0ef;
+    color: #01161e;
+  }
+  
+  .enroll-button {
+    background-color: #90e0ef;
+    border: none;
+    margin-right: 16px;
+    transition: 0.3s;
+  }
+  
+  .enroll-button:hover {
+    background-color: #8de1f0c9;
+    color: white;
+  }
+  </style>
+  
