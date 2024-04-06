0d21583b1ce1c9f4f8a5d7c0ff5dbc8bd315f4ed
liamdichiro <115846729+liamdichiro@users.noreply.github.com>
2024-04-06 17:07:43 -0400

    Update registrationPage.vue

--- sportscenterapp-frontend/src/pages/dashboardPages/registrationPage.vue
+++ sportscenterapp-frontend/src/pages/dashboardPages/registrationPage.vue
@@ -1,22 +1,111 @@
 <template>
     <Dashboard>
         <h1>PAGE FOR SEEING ALL CUSTOMER REGISTRATIONS</h1>
-        <h2>Accessible by customer</h2>
+        <h2>Accessible by cusstomer</h2>
         <p> The customer can see/cancel his registrations</p>
+        <registrationtable :registrations="registrations" @cancel-registration="cancelRegistration" />
+
     </Dashboard>
   </template>
-  
+
   <script>
+  import axios from 'axios';
   import Dashboard from '@/pages/Dashboard'
-  
-  export default {
-    name: 'registrationPage',
-    components: {
-      Dashboard
-    }
+  import registrationtable from '@/components/registrationtable'
+
+export default {
+  name: 'registrationPage',
+  components: {
+    Dashboard,
+    registrationtable
+  },
+  data() {
+    return {
+      registrations: []
+    };
+  },
+  mounted(){
+    this.fetchRegistrations();
+  },
+  methods: {
+    addRegistration(registration) {
+
+      this.fetchRegistrations();
+    },
+    cancelRegistration(index) {
+      this.registrations.splice(index, 1);
+    },
+    fetchRegistrations() {
+        axios.get('http://localhost:8080/registration/')
+          .then(res => {
+            this.registrations = res.data;
+          })
+          .catch(err => {
+            console.log(err.response.data);
+          })
   }
-  </script>
+}
+}
+</script>
 
-  <style>
+<style scoped>
 
-  </style>
+
+.button-container {
+  display: flex;
+  justify-content: flex-end;
+  margin-right: 24px;
+  margin-top: 24px;
+
+}
+
+button {
+  background-color: #77DD77;
+  color: white;
+  border-radius: 4px;
+  width: 256px;
+  height: 48px;
+  border: none;
+  font-weight: bold;
+}
+
+button:hover {
+  background-color: #2aaa2a;
+}
+
+
+.modal-overlay {
+  position: fixed;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background-color: rgba(0, 0, 0, 0.5); /* Semi-transparent black overlay */
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  z-index: 999; /* Ensure the modal is on top of other content */
+}
+
+/* Styles for the modal content */
+.modal-content {
+  padding: 20px;
+  border-radius: 8px;
+  min-width: 400px;
+  max-width: 700px;
+  max-height: 800px;
+  height: auto;
+  overflow-y: auto; /* Enable scrolling if the content exceeds the height */
+  position: absolute;
+  top: 50;
+  left: 35%;
+}
+
+/* Close button styles */
+.close-button {
+  position: absolute;
+  top: 10px;
+  right: 10px;
+  cursor: pointer;
+}
+</style>
